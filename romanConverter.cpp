4c7c372b542b92bcfc6d416ca5ec84d256bd7490
Roberta <robertacs42@gmail.com>
2019-04-18 19:43:56 -0300

    teste1 - passed

--- romanConverter.cpp
+++ romanConverter.cpp
@@ -1,7 +1,39 @@
 #include "romanConverter.hpp"
 
+#include <iostream>
+
+
+using namespace std;
+
 int romanConverter(char string[]){
+    int i = 0;
     int convertido = 0;
+
+        switch (string[i]){
+		case 'I':
+			convertido = 1;
+            break;
+		case 'V': 
+			convertido =  5;
+            break;
+		case 'X':
+			convertido =  10;
+            break;
+		case 'L':
+			convertido =  50;
+            break;
+		case 'C':
+			convertido =  100;
+            break;
+		case 'D':
+			convertido =  500;
+            break;
+		case 'M':
+			convertido =  1000;
+            break;
+		default:
+			convertido =  -1;
+	    }
 
     return convertido;
 }
