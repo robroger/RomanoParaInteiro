740ff9c50048fe30c405a59d1a6262802dbb0300
Roberta <robertacs42@gmail.com>
2019-04-18 22:23:37 -0300

    teste valores numeros invalidos - passed

--- romanConverter.cpp
+++ romanConverter.cpp
@@ -1,7 +1,5 @@
 #include "romanConverter.hpp"
 
-#include <iostream>
-
 int valordeumchar (char caractere) {	// Switch case para definir tipos básicos
 	switch (caractere){
 		case 'I':
@@ -36,19 +34,31 @@ int romanConverter(char string[]){
     int convertido = 0;		// Resultado da conversão para inteiro
 	char caractere_atual;
 	char caractere_pos;
+	
 
 	while(i<strlen(string) && string[i] != '\0') {	// Loop para percorrer a string
 		char caractere_atual = string[i];
 		char caractere_pos = string[i+1];
 
+		//Checa valores invalidos
 		if(valordeumchar(caractere_atual) == -1){
 			return -1;
-		} 
-		
-		if (valordeumchar(caractere_atual) >= valordeumchar(caractere_pos)){ // If para tipos que precisam de soma
+		}
+
+		//Checa números inválidos
+		if (string[i] == 'V' && string[i+1] == 'V' || string[i] == 'L' && string[i+1] == 'L' || string[i] == 'D' && string[i+1] == 'D') {
+			return -1;
+		}
+		//Checa números inválidos
+		if (string[i] == string[i+1] && string[i] == string[i+2] && string[i] == string[i+3]) {
+			return -1;
+		}		
+
+		//If para tipos válidos que precisam de soma
+		if (valordeumchar(caractere_atual) >= valordeumchar(caractere_pos)){ 
 			convertido = convertido + valordeumchar(caractere_atual);
 			i++;
-		} else if (valordeumchar(caractere_atual) < valordeumchar(caractere_pos)) { // If para tipos que precisam de subtração
+		} else if (valordeumchar(caractere_atual) < valordeumchar(caractere_pos)) { //If para tipos válidos que precisam de subtração
 			convertido = convertido + valordeumchar(caractere_pos) - valordeumchar(caractere_atual); 
 			i = i + 2;
 		}
--- romanConverter.hpp
+++ romanConverter.hpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 int romanConverter(char string[]); 
 
 #endif
